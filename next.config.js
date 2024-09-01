5392730e34e07aa21a0d587a41c9f7f81a889d8a
Mudra Rawal <mrawal@uritechnology.com>
2024-09-01 18:54:48 -0500

    add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' }
+    };
+  }
 }
 
 module.exports = nextConfig
