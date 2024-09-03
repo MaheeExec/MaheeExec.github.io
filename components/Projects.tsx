34552b2cfdb4914f5f17d30beaa954d06ed3315a
Mudra Rawal <mrawal@uritechnology.com>
2024-09-02 20:20:24 -0500

    add descriptions

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -44,64 +44,48 @@ export default function Writing() {
           </Heading>
           <p>
             {" "}
-            Throughout my school days, I have demonstrated strong leadership
-            skills through various roles, including team management, project
-            leadership, and strategic planning. My leadership experience has
-            been marked by successful project outcomes, team development, and
-            strategic improvements. Imagine being the captain of a ship, but
-            your ship is a kitchen, and your crew is a team of six enthusiastic
-            chefs. The challenge? A national &aposCooking Without Fire&apos
-            competition. With a sprinkle of guidance, a dash of motivation, and
-            a generous helping of teamwork, we navigated through the choppy
-            waters of culinary challenges. Leading this team wasn&apost just
-            about winning, it was about blending our skills to create something
-            deliciously extraordinary. That&aposs the essence of leadership!
+            Photography allows me to capture moments and explore the world
+            through a creative lens, finding beauty in everyday life. It&aposs a
+            way to express myself artistically while preserving memories that
+            tell a story.
           </p>
         </div>
         <div className="grid grid-cols-3 gap-4">
           <img
-            className="col-span-1 row-span-1 h-75 object-cover"
-            src="/images/photo1.jpg"
+            className="h-75 col-span-1 row-span-1 object-cover"
+            src="/images/photo1.JPG"
             alt="Photo 1"
           />
           <img
-            className="col-span-1 row-span-1 h-75 object-cover"
-            src="/images/photo2.jpg"
+            className="h-75 col-span-1 row-span-1 object-cover"
+            src="/images/photo2.JPG"
             alt="Photo 2"
           />
           <img
-            className="col-span-1 row-span-1 h-75 object-cover"
-            src="/images/photo3.png"
+            className="h-75 col-span-1 row-span-1 object-cover"
+            src="/images/photo3.PNG"
             alt="Photo 3"
           />
           <img
-            className="col-span-2 row-span-1 h-75 object-cover"
-            src="/images/photo4.png"
+            className="h-75 col-span-2 row-span-1 object-cover"
+            src="/images/photo4.PNG"
             alt="Photo 4"
           />
           <img
-            className="col-span-1 row-span-1 h-75 object-cover"
-            src="/images/photo5.png"
+            className="h-75 col-span-1 row-span-1 object-cover"
+            src="/images/photo5.PNG"
             alt="Photo 5"
           />
         </div>
         <div>
           <Heading size="sm" dark className="font-extrabold">
-            <SplitText text="Skill-3 Leadership" />
+            <SplitText text="Drawing" />
           </Heading>
           <p>
-            Throughout my school days, I have demonstrated strong leadership
-            skills through various roles, including team management, project
-            leadership, and strategic planning. My leadership experience has
-            been marked by successful project outcomes, team development, and
-            strategic improvements. Imagine being the captain of a ship, but
-            your ship is a kitchen, and your crew is a team of six enthusiastic
-            chefs. The challenge? A national &aposCooking Without Fire&apos
-            competition. With a sprinkle of guidance, a dash of motivation, and
-            a generous helping of teamwork, we navigated through the choppy
-            waters of culinary challenges. Leading this team wasn&apost just
-            about winning, it was about blending our skills to create something
-            deliciously extraordinary. That&aposs the essence of leadership!
+            Drawing lets me bring my imagination to life, turning ideas into
+            visual art that reflects my thoughts and emotions. It’s a relaxing
+            and fulfilling way to explore creativity and improve my artistic
+            skills.
           </p>
         </div>
 
